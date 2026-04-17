36c292b1b4250182f1d6c681d51c064ef9f59b89
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 21:52:15 +0300

    drop ide deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,45 @@
  * @since 2023-03-30
  */
 open module mobi.hsz.idea.gitignore {
+    requires consulo.application.api;
+    requires consulo.application.content.api;
+    requires consulo.application.ui.api;
+    requires consulo.base.icon.library;
+    requires consulo.code.editor.api;
+    requires consulo.color.scheme.api;
+    requires consulo.component.api;
+    requires consulo.configurable.api;
+    requires consulo.container.api;
+    requires consulo.disposer.api;
+    requires consulo.document.api;
+    requires consulo.file.chooser.api;
+    requires consulo.file.editor.api;
+    requires consulo.file.template.api;
     requires consulo.ide.api;
+    requires consulo.index.io;
+    requires consulo.language.api;
+    requires consulo.language.editor.api;
+    requires consulo.language.impl;
+    requires consulo.localize.api;
+    requires consulo.logging.api;
+    requires consulo.module.api;
+    requires consulo.module.content.api;
+    requires consulo.process.api;
+    requires consulo.project.api;
+    requires consulo.project.ui.api;
+    requires consulo.project.ui.view.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.ui.ex.awt.api;
+    requires consulo.undo.redo.api;
+    requires consulo.util.collection;
+    requires consulo.util.dataholder;
+    requires consulo.util.io;
+    requires consulo.util.jdom;
+    requires consulo.util.lang;
+    requires consulo.version.control.system.api;
+    requires consulo.virtual.file.status.api;
+    requires consulo.virtual.file.system.api;
 
     requires org.apache.commons.lang3;
 
